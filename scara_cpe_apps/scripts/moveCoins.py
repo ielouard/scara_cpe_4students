3b4220a47e0ea591109f709e8f7b095582b0ba67
ielouard <ibrahim@hoplitepower.com>
2018-11-21 13:43:10 +0100

    last commit

--- scara_cpe_apps/scripts/moveCoins.py
+++ scara_cpe_apps/scripts/moveCoins.py
@@ -10,10 +10,11 @@ from std_msgs.msg import Bool
 import time
 
 class Move_Coins:
-    #TODO define A AND B
     A = [-0.73,0.08]
     B = [0.035,0.14]
     grip_pub=rospy.Publisher('grip', Bool, queue_size=1)
+    scene = moveit_commander.PlanningSceneInterface()
+    robot = moveit_commander.RobotCommander()
 
     def __init__(self):
         rospy.loginfo('starting init')
@@ -21,6 +22,19 @@ class Move_Coins:
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.Subscriber('detect_metal', Bool, self.callback)
         self.group = moveit_commander.MoveGroupCommander("scara_cpe_group")
+
+    def init_box(self,box_name,x,y,s):
+        self.scene.remove_world_object(box_name)
+        time.sleep(2)
+        rospy.loginfo("init_box")
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.frame_id= "end_link"
+        box_pose.pose.orientation.w= 1.0
+        box_pose.pose.position.x= x
+        box_pose.pose.position.y= y
+        box_pose.pose.position.z= 0.025
+        self.scene.add_box(box_name, box_pose, size=s)
+        rospy.loginfo("end of init box")
 
     def set_coordonnee(self,coord):
         self.group.clear_pose_targets()
@@ -35,6 +49,9 @@ class Move_Coins:
 
 if __name__ == '__main__':
     scara = Move_Coins()
+    scara.init_box("box", 0.051, 0.007, (0.04,0.04,0.04))
+    scara.init_box("coin", 0.0, 0.035, (0.02,0.02,0.02))
+    scara.scene.attach_box(scara.group.get_end_effector_link(),"coin")
     release=Bool()
     release.data=True
     scara.set_coordonnee(scara.A)
